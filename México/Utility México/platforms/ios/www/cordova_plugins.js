c756c561e91c3cc8cd242e8314d50da905d0b10c
Ernesto Magaña <ernesto@kokonutstudio.com>
2016-10-01 00:33:36 -0500

    - Se organizó el repositorio por países - Cada país cuenta con una plataforma iOS y una Android - Cada plataforma cuenta con su carpeta "www", que tienen lo mismo cada una, pero se necesita tener esa estructura porque son "proyectos independientes" - Adicional hay una carpeta "www" por país, así que hay 6 carpetas "www" con la misma cosa

---
+++ México/Utility México/platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,35 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/org.apache.cordova.statusbar/www/statusbar.js",
+        "id": "org.apache.cordova.statusbar.statusbar",
+        "clobbers": [
+            "window.StatusBar"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "id": "cordova-plugin-dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "org.apache.cordova.statusbar": "0.1.4",
+    "cordova-plugin-whitelist": "1.3.0",
+    "cordova-plugin-dialogs": "1.3.0",
+    "cordova-plugin-inappbrowser": "1.5.0"
+}
+// BOTTOM OF METADATA
+});
